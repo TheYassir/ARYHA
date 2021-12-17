eb9f09a80db6490ef531e1df0e47ba8534e0fd34
Yassir ARAS <arasyassir@gmail.com>
2021-12-17 12:07:33 +0100

    Ajout Connexion Inscription

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -16,28 +16,84 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
+         // Si getUser() renvoi des données, cela veut dire que l'internaute est authentifié, il n'a rien à faire sur la route '/register', on le redirige vers la route de connexion '/blog'
+        if($this->getUser())
+        {
+            return $this->redirectToRoute ('shop');
+        }
+
         $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
+
+        $form = $this->createForm(RegistrationFormType::class, $user, [
+            'userRegistration' => true // on precise dans quelle condition on entre, dans la classe registrationFormType pour afficher un formulaire en particulier, la classe contient plusieur formulaire
+        ]);
+
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-            $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            // fait appel a l'objet $userPasswordHasher de la classe UserPasswordHasherInterface afin d'encoder le mot de passe en BDD
+            // En argument on lui fournit l'objet entité dans lequel nous allons encoder un élément ($user) et on lui fournit le mot de passe saisi dans le formulaire a encoder
+            $hash = $userPasswordHasher->hashPassword(
+                $user,
+                $form->get('password')->getData()
             );
+
+            //dd($hash);
+
+            // on transmet au setter du password la clé de hachage à enregistrer en bdd
+            $user->setPassword($hash);
+
+            $this->addFlash('success', "Félicitation ! Vous êtes maintenant inscrit sur le site !");
 
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('shop');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/profil', name: 'app_profil')]
+    public function userProfil(): Response
+    {
+        if(!$this->getUser())
+        {
+            return $this->redirectToRoute('app_login');
+        }
+        $user= $this->getUser();
+
+        return $this->render('registration/profil.html.twig', [
+            'user' => $user
+        ]);
+
+    }
+
+    #[Route('/profil/{id}/edit', name: 'app_profil_edit')]
+    public function userProfilEdit(User $user, Request $request, EntityManagerInterface $manager): Response
+    {
+        $formUpdate = $this->createForm(RegistrationFormType::class, $user, [
+            'userUpdate' => true
+        ]);
+
+        $formUpdate->handleRequest($request);
+        
+        if($formUpdate->isSubmitted() && $formUpdate->isValid())
+        {
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash('success', 'Vous avez modifié vos informations, merci de vous authentifié de nouveau');
+
+            return $this->redirectToRoute('app_logout');
+        }
+
+        return $this->render('registration/profil_edit.html.twig', [
+            'formUpdate' => $formUpdate->createView()
         ]);
     }
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,55 +4,193 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+        if($options['userRegistration'] == true)
+        {
+            $builder
+                ->add('email', TextType::class, [
+                    'required' => true,
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => "Veuillez renseigner votre email"
+                        ])
+                    ]
+                ])
+                ->add('pseudo', TextType::class, [
+                    'required' => true,
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => "Veuillez renseigner votre pseudo"
+                        ])
+                    ]
+                ])
+                ->add('prenom', TextType::class, [
+                    'required' => true,
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => "Veuillez renseigner votre prenom"
+                        ])
+                    ]
+                ])
+                ->add('nom', TextType::class, [
+                    'required' => true,
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => "Veuillez renseigner votre nom"
+                        ])
+                    ]
+                ])
+                ->add('sexe', ChoiceType::class, ['choices' => [ 'Homme' => 'Homme', 'Femme' => 'Femme'], 'expanded' => false, 'multiple' => false, 'label' => "Civilité",])
+
+                ->add('adresse', TextType::class, [
+                    'required' => true,
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => "Veuillez renseigner votre adresse"
+                        ])
+                    ]
+                ])
+                ->add('ville', TextType::class, [
+                    'required' => true,
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => "Veuillez renseigner votre ville"
+                        ])
+                    ]
+                ])
+                ->add('codePostal', NumberType::class, [
+                    'required' => true,
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => "Veuillez renseigner votre code postale"
+                        ])
+                    ]
+                ])
+                ->add('telephone', NumberType::class, [
+                    'required' => true,
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => "Veuillez renseigner votre numéro de téléphone"
+                        ])
+                    ]
+                ])
+                ->add('password', RepeatedType::class, [
+                    'type' => PasswordType::class,
+                    'required' => true,
+                    'invalid_message' => "les mots de passe ne correspondent pas",
+                    'options' => [
+                        'attr' => [
+                            'class' => 'password-field'
+                        ]
+                    ],
+                    'first_options' => [
+                        'label' => 'Mot de passe'
+                    ],
+                    'second_options' => [
+                        'label' => "Confirmer votre mot de passe"
+                    ],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez renseigner votre mot de passe'
+                        ]),
+                        new Length([
+                            'min' => 8,
+                            'minMessage' => "Votre mot de passe doit contenir au minimum 8 caractères"
+                        ])
+                    ]
+                ])
+            ;
+        }
+        elseif($options['userUpdate'] == true)
+        {
+            $builder
+            ->add('email', TextType::class, [
+                'required' => false,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
+                        'message' => "Veuillez renseigner votre email"
+                    ])
+                ]
             ])
-        ;
+            ->add('prenom', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Veuillez renseigner votre prenom"
+                    ])
+                ]
+            ])
+            ->add('nom', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Veuillez renseigner votre nom"
+                    ])
+                ]
+            ])
+            ->add('adresse', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Veuillez renseigner votre adresse"
+                    ])
+                ]
+            ])
+            ->add('ville', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Veuillez renseigner votre ville"
+                    ])
+                ]
+            ])
+            ->add('codePostal', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Veuillez renseigner votre code postale"
+                    ])
+                ]
+            ]);
+        }
+        elseif($options['userUpdateBack'] == true)
+        {
+            $builder
+            ->add('roles', ChoiceType::class, [
+                    'choices' => [
+                        'Utilisateur' => '',
+                        'Administrateur' => 'ROLE_ADMIN'
+                    ],
+                    'expanded' => true,
+                    'multiple' => true,
+                    'label' => "Définir le role de l'utilisateur",
+                ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'userRegistration' => false,
+            'userUpdateBack' => false,
+            'userUpdate' => false
         ]);
     }
-
-    
 }
-
